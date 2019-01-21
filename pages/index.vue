2f7fabd72335c02cf69da92c91cdbc9397c9a366
Adam Harpur <adam@harpur.io>
2019-01-21 17:26:59 -0600

    refactor: index.vue tidy

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,39 @@
 <template>
-  <!--=============================================================================
-				Front Section
-  ===============================================================================-->
   <section class="front-section">
     <div id="diamond-wrapper" class="container">
       <div class="transition-mask"></div>
 
       <div class="front-person-img">
-        <!--person's image-->
         <img src="/assests/img/me.png" alt="portfolio">
-        <!--/person's image-->
       </div>
 
-      <!--person's titles-->
       <div class="front-person-titles">
-        <!--title1-->
         <span class="t1">Creator</span>
-        <!--/title1-->
-        <!--title2-->
+        
         <span class="t2">Programmer</span>
-        <!--/title2-->
-        <!--title3-->
+        
         <span class="t3">Maker</span>
-        <!--/title3-->
       </div>
-      <!--/person's titles-->
       <nav class="front-person-links">
-        <!--
-							navigation links, data-section attribute points towards the section with id to be opened. 
-        -->
         <ul>
           <li>
-            <nuxt-link to="/about">About (internal link that belongs to the Nuxt App)</nuxt-link>
-            <a href="#" data-section="about">About Me</a>
+            <nuxt-link to="/about">About</nuxt-link>
           </li>
 
           <li>
-            <a href="#" data-section="resume">Resume</a>
+            <nuxt-link to="/resume">Resume</nuxt-link>
           </li>
 
           <li>
-            <a href="#" data-section="services">Services</a>
+            <nuxt-link to="/services">Services</nuxt-link>
           </li>
 
           <li>
-            <a href="#" data-section="portfolio">Portfolio</a>
+            <nuxt-link to="/portfolio"></nuxt-link>
           </li>
 
           <li>
             <a href="#" data-section="blog">My Blog</a>
-          </li>
-
-          <li>
-            <a href="#" data-section="contact">Contact</a>
-          </li>
-
-          <li>
-            <a href="#">Hire Me!</a>
           </li>
         </ul>
       </nav>
@@ -66,7 +43,4 @@
       </div>
     </div>
   </section>
-  <!--=============================================================================
-				/Front Section
-  ===============================================================================-->
 </template>
