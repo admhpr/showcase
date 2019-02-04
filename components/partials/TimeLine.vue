b2a7b76636ef5f37f087d103d7529961c481e57d
Adam Harpur <adam@harpur.io>
2019-02-03 19:27:22 -0600

    feat: added time line component

---
+++ components/partials/TimeLine.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="timeline-block section-block">
+    <div class="container">
+      <ul class="timeline">
+        <li class="timeline-header">
+          <h4>Education</h4>
+        </li>
+
+        <li>
+          <div class="timeline-desc">
+            <h4>2005-2007</h4>
+          </div>
+
+          <div class="timeline-content">
+            <h4>Masters Degree</h4>
+            <span>@CSS College Larkana</span>
+
+            <p>
+              Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
+              vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+            </p>
+          </div>
+        </li>
+
+        <li class="inverse">
+          <div class="timeline-desc">
+            <h4>2005-2007</h4>
+          </div>
+
+          <div class="timeline-content">
+            <h4>Masters Degree</h4>
+            <span>@CSS College Larkana</span>
+
+            <p>
+              Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
+              vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+            </p>
+          </div>
+        </li>
+
+        <li>
+          <div class="timeline-desc">
+            <h4>2005-2007</h4>
+          </div>
+
+          <div class="timeline-content">
+            <h4>Masters Degree</h4>
+            <span>@CSS College Larkana</span>
+
+            <p>
+              Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
+              vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+            </p>
+          </div>
+        </li>
+
+        <li class="timeline-header">
+          <h4>Experience</h4>
+        </li>
+
+        <li>
+          <div class="timeline-desc">
+            <h4>2005-2007</h4>
+          </div>
+
+          <div class="timeline-content">
+            <h4>Web Designer</h4>
+            <span>@Themeforest</span>
+
+            <p>
+              Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
+              vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+            </p>
+          </div>
+        </li>
+
+        <li class="inverse">
+          <div class="timeline-desc">
+            <h4>2005-2007</h4>
+          </div>
+
+          <div class="timeline-content">
+            <h4>Web Developer</h4>
+            <span>@CodeCanyon</span>
+
+            <p>
+              Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
+              vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+            </p>
+          </div>
+        </li>
+
+        <li>
+          <div class="timeline-desc">
+            <h4>2005-2007</h4>
+          </div>
+
+          <div class="timeline-content">
+            <h4>Blogger</h4>
+            <span>@blogspot</span>
+
+            <p>
+              Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
+              vituperata suscipiantur et, et fabellas explicari adipiscing quo.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,127 +1,18 @@
 <template>
-  <div>
-    <section id="resume" class="resume-section section-show">
-      <section-title title="My Resume"></section-title>
-      <div class="timeline-block section-block">
-        <div class="container">
-          <ul class="timeline">
-            <li class="timeline-header">
-              <h4>Education</h4>
-            </li>
-
-            <li>
-              <div class="timeline-desc">
-                <h4>2005-2007</h4>
-              </div>
-
-              <div class="timeline-content">
-                <h4>Masters Degree</h4>
-                <span>@CSS College Larkana</span>
-
-                <p>
-                  Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
-                  vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-                </p>
-              </div>
-            </li>
-
-            <li class="inverse">
-              <div class="timeline-desc">
-                <h4>2005-2007</h4>
-              </div>
-
-              <div class="timeline-content">
-                <h4>Masters Degree</h4>
-                <span>@CSS College Larkana</span>
-
-                <p>
-                  Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
-                  vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-                </p>
-              </div>
-            </li>
-
-            <li>
-              <div class="timeline-desc">
-                <h4>2005-2007</h4>
-              </div>
-
-              <div class="timeline-content">
-                <h4>Masters Degree</h4>
-                <span>@CSS College Larkana</span>
-
-                <p>
-                  Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
-                  vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-                </p>
-              </div>
-            </li>
-
-            <li class="timeline-header">
-              <h4>Experience</h4>
-            </li>
-
-            <li>
-              <div class="timeline-desc">
-                <h4>2005-2007</h4>
-              </div>
-
-              <div class="timeline-content">
-                <h4>Web Designer</h4>
-                <span>@Themeforest</span>
-
-                <p>
-                  Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
-                  vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-                </p>
-              </div>
-            </li>
-
-            <li class="inverse">
-              <div class="timeline-desc">
-                <h4>2005-2007</h4>
-              </div>
-
-              <div class="timeline-content">
-                <h4>Web Developer</h4>
-                <span>@CodeCanyon</span>
-
-                <p>
-                  Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
-                  vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-                </p>
-              </div>
-            </li>
-
-            <li>
-              <div class="timeline-desc">
-                <h4>2005-2007</h4>
-              </div>
-
-              <div class="timeline-content">
-                <h4>Blogger</h4>
-                <span>@blogspot</span>
-
-                <p>
-                  Consul latine iudicabit eu vel. Cu has animal eruditi voluptatibus. Eu volumus explicari sed. Mel mutat
-                  vituperata suscipiantur et, et fabellas explicari adipiscing quo.
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <section-container title="Resume">
+    <div slot="content">
+      <time-line></time-line>
       <info-diamonds title="Hobbies" :data="hobbies"/>
       <skills/>
-    </section>
-  </div>
+    </div>
+  </section-container>
 </template>
 
 <script>
 import InfoDiamonds from "~/components/sections/InfoDiamonds";
-import SectionTitle from "~/components/layout/SectionTitle";
 import Skills from "~/components/sections/Skills";
 import SocialLinks from "~/components/partials/SocialLinks";
+import TimeLine from "~/components/partials/TimeLine";
 import hobbies from "~/data/hobbies";
 export default {
   data: function() {
@@ -131,9 +22,9 @@ export default {
   },
   components: {
     InfoDiamonds,
-    SectionTitle,
     Skills,
-    SocialLinks
+    SocialLinks,
+    TimeLine
   }
 };
 </script>
