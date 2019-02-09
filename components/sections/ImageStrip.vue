1ff0cf9ffff61ec4b90c8ec050575c5895eaac32
Adam Harpur <adam@harpur.io>
2019-02-09 17:18:32 -0600

    feat: added image strip component

--- components/sections/ImageStrip.vue
+++ components/sections/ImageStrip.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="image-strip">
-    <img v-for="img in images" :key="img.src" :src="img.src" :alt="img.alt">
+  <div class="row">
+    <div class="image-strip">
+      <img class="img-thumbnail" v-for="img in images" :key="img.src" :src="img.src" :alt="img.alt">
+    </div>
   </div>
 </template>
 
---
+++ data/image-strips/set-one.js
@@ -0,0 +1,14 @@
+const base = '../assets/img/featured/'
+export default [{
+    src: `${base}1.jpeg`,
+    alt: ''
+}, {
+    src: `${base}1.jpeg`,
+    alt: ''
+}, {
+    src: `${base}1.jpeg`,
+    alt: ''
+}, {
+    src: `${base}1.jpeg`,
+    alt: ''
+}, ]
