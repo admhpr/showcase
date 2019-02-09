2be22c11181f2790be2107d733afa4fea76bbe13
Adam Harpur <adam@harpur.io>
2019-02-09 15:37:16 -0600

    feat: contact form submit

--- components/partials/ContactForm.vue
+++ components/partials/ContactForm.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    <form id="contact-form" class="col-md-8 offset-md-2 contact-form">
+    <form
+      id="contact-form"
+      class="col-md-8 offset-md-2 contact-form"
+      v-on:submit.prevent="onSubmit"
+    >
       <div class="form-group">
-        <input class="form-control" v-model="name" placeholder="Name">
+        <input class="form-control" v-model.trim="name" placeholder="Name">
         <div class="help-block with-errors"></div>
       </div>
 
       <div class="form-group">
-        <input class="form-control" v-model="email" placeholder="Email">
+        <input class="form-control" v-model.trim="email" placeholder="Email">
       </div>
 
       <div class="form-group">
-        <textarea class="form-control" v-model="message" placeholder="Message"></textarea>
+        <textarea class="form-control" v-model.trim="message" placeholder="Message"></textarea>
       </div>
 
       <div class="form-group text-center">
-        <button class="symp-btn">Send Message</button>
+        <button type="submit" class="symp-btn">Send Message</button>
       </div>
     </form>
   </div>
@@ -29,6 +33,20 @@ export default {
       email: "",
       message: ""
     };
+  },
+  methods: {
+    onSubmit: function() {
+      fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.$data)
+      })
+        .then(res => res.json())
+        .then(res => console.log(res));
+    }
   }
 };
 </script>
