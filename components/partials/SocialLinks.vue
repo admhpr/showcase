8d75b48ddb2f72f2166a88fa6f71bb7c6d6e1030
Adam Harpur <adam@harpur.io>
2019-02-10 06:33:29 -0600

    feat: added social links

--- components/partials/SocialLinks.vue
+++ components/partials/SocialLinks.vue
@@ -3,13 +3,13 @@
     <a href="https://www.ecosia.org/search?q=harps116">
       <font-awesome-icon :icon="['fas', 'globe-europe']" size="lg"/>
     </a>
-    <a href>
+    <a href="https://github.com/harps116">
       <font-awesome-icon :icon="['fab', 'github']" size="lg"/>
     </a>
-    <a href>
+    <a href="https://twitter.com/@harps116">
       <font-awesome-icon :icon="['fab', 'twitter']" size="lg"/>
     </a>
-    <a href>
+    <a href="https://www.linkedin.com/in/adam-harpur/">
       <font-awesome-icon :icon="['fab', 'linkedin']" size="lg"/>
     </a>
   </div>
