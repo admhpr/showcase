f993741b25ff2f52ea17ed2e1f4420836f566979
Adam Harpur <adam@harpur.io>
2019-02-11 19:42:58 -0600

    feat: setup contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,17 +1,21 @@
 
 <template>
   <section-container title="Contact">
-    <div slot="content"></div>
+    <div slot="content">
+      <contact-form/>
+    </div>
   </section-container>
 </template>
 
 <script>
+import ContactForm from "~/components/partials/ContactForm";
 import SectionContainer from "~/components/containers/Section";
 export default {
   data: function() {
     return {};
   },
   components: {
+    ContactForm,
     SectionContainer
   }
 };
