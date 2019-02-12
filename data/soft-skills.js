ef8bf766fb9caea36865c7385b5fbffdba559def
Adam Harpur <adam@harpur.io>
2019-02-11 20:11:57 -0600

    chore: update data

--- data/soft-skills.js
+++ data/soft-skills.js
@@ -8,26 +8,26 @@ export default [{
     },
     {
         title: "Problem Solver",
-        info: "lorem blah blah blah"
+        info: "At it's core writing code is solving problems on a micro or macro scale."
     },
     {
         title: "Hard Working",
-        info: "lorem blah blah blah"
+        info: "I work hard, on a mission to be better, I put in the time to improve my skill set."
     },
     {
         title: "Reliable",
-        info: "lorem blah blah blah"
+        info: "I respect the time of others. I don't show up late or take unnecessary days off"
     },
     {
         title: "Honest",
-        info: "lorem blah blah blah"
+        info: "I always seek the truth, good or bad. I believe it's honesty that improves the world around us."
     },
     {
         title: "Passionate",
-        info: "lorem blah blah blah"
+        info: "Code means a lot to me, I feel it is a very important tool in this current digital age. I'm passionate about the craft."
     },
     {
         title: "Critical Thinker",
-        info: "lorem blah blah blah"
+        info: "Being able to take an objective view on a given situation is vital to making better decisions"
     },
 ]
