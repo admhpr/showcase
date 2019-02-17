fa139548f4d1d97c7b66cb34219af77e0c88321e
Adam Harpur <adam@harpur.io>
2019-02-17 13:52:51 -0600

    feat: error handling for form

--- components/partials/ContactForm.vue
+++ components/partials/ContactForm.vue
@@ -6,21 +6,36 @@
       v-on:submit.prevent="onSubmit"
     >
       <div class="form-group">
-        <input class="form-control" v-model.trim="form.name" placeholder="Name">
+        <input
+          :class="{'form-control': true, 'input-error': this.error}"
+          v-model.trim="form.name"
+          placeholder="Name"
+        >
         <div class="help-block with-errors"></div>
       </div>
 
       <div class="form-group">
-        <input class="form-control" v-model.trim="form.email" placeholder="Email">
+        <input
+          :class="{'form-control': true, 'input-error': this.error}"
+          v-model.trim="form.email"
+          placeholder="Email"
+        >
       </div>
 
       <div class="form-group">
-        <textarea class="form-control" v-model.trim="form.message" placeholder="Message"></textarea>
+        <textarea
+          :class="{'form-control': true, 'input-error': this.error}"
+          v-model.trim="form.message"
+          placeholder="Message"
+        ></textarea>
       </div>
 
       <div class="form-group text-center">
         <button type="submit" class="symp-btn">Send Message</button>
       </div>
+      <div
+        :class="{ 'text-center' : true, 'contact-message' : this.message.length > 0 && !hideMessage, 'contact-message-hidden': hideMessage}"
+      >{{this.message}}</div>
     </form>
   </div>
 </template>
@@ -33,6 +48,9 @@ export default {
   methods: {
     initState: function() {
       return {
+        error: false,
+        hideMessage: true,
+        message: "",
         form: {
           name: "",
           email: "",
@@ -41,9 +59,10 @@ export default {
       };
     },
     resetForm: function() {
-      this.name = "";
-      this.email = "";
-      this.message = "";
+      this.error = false;
+      Object.keys(this.form).forEach(key => {
+        this.form[key] = "";
+      });
     },
     onSubmit: function() {
       fetch("/api/contact", {
@@ -52,15 +71,20 @@ export default {
           Accept: "application/json, text/plain, */*",
           "Content-Type": "application/json"
         },
-        body: JSON.stringify(this.$data)
+        body: JSON.stringify(this.$data.form)
       })
         .then(res => res.json())
-        .then(res => {
-          if (res.status !== "success") {
-            console.log("here");
+        .then(json => {
+          if (json.status !== "success") {
+            this.error = true;
             return;
           }
           this.resetForm();
+          this.message = json.message;
+          this.hideMessage = false;
+          setTimeout(() => {
+            this.hideMessage = true;
+          }, 3000);
         });
     }
   }
