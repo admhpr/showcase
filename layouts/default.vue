ba9bb30d30556ee62dcb9287df9f7ad42df20087
Adam Harpur <adam@harpur.io>
2019-02-19 17:27:54 -0600

    chore: updated css for vue-github

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,5 +19,5 @@ export default {
 
 <style>
 @import url("https://cdnjs.cloudflare.com/ajax/libs/octicons/3.5.0/octicons.min.css");
-@import url("https://unpkg.com/vue-github@0.9.19/dist/vueGithub.css");
+@import url("https://unpkg.com/vue-github@0.9.20/dist/vueGithub.css");
 </style>
