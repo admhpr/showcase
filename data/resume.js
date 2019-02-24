1b88e190b22465f5a4c15130d3bb186b4800022a
Adam Harpur <adam@harpur.io>
2019-02-24 08:51:05 -0600

    chore: update verbage

--- data/resume.js
+++ data/resume.js
@@ -2,18 +2,18 @@ export default [{
         title: "Education",
         data: [{
                 title: "ITS Associate of Applied Science",
-                info: "Focus on web and software development as well as design",
+                info: "Focus on web and software development as well as design.",
                 location: "Western Technical College",
                 meta: "May 2016 - May 2018"
             },
             {
                 title: "Various Courses",
-                info: "I have done a number of courses on Frontend Master, Udemy and Udacity",
+                info: "I have done a number of courses on Frontend Master, Udemy and Udacity.",
                 meta: "2016 - Present"
             },
             {
                 title: "Travelling",
-                info: "I have spent a large portion of my life travelling which has taught me many things",
+                info: "I have spent a large portion of my life travelling which has taught me many things about people and the world.",
                 location: "America, Australia, Europe",
                 meta: "2008 - Present",
             },
@@ -24,18 +24,18 @@ export default [{
         data: [{
                 title: "FullStack Web Developer",
                 location: "Coulee Tech Inc",
-                info: "Working with JavaScript, TypeScript, Node and couchDB",
+                info: "Working with JavaScript, TypeScript, React, Node and CouchDB.",
                 meta: "Aug 2017 - Present"
             },
             {
                 title: "Computer Tech",
                 location: "Orange Computer",
-                info: "Hardware upgrades, server maintenance",
+                info: "Hardware upgrades, Linux server maintenance.",
                 meta: "Dec 2016 - Jan 2018"
             },
             {
                 title: "Various other non tech jobs",
-                info: "In an other life I worked as a Cook, Painter, Bartender, Farmhand, Deckhand",
+                info: "In an other life I worked as a Cook, Painter, Bartender, Farmhand, Deckhand everyone has taught me something about how to work effectively.",
                 meta: "2008 - 2016"
             },
         ]
@@ -45,13 +45,13 @@ export default [{
         data: [{
                 title: "La Crosse Area Meet Up",
                 location: "La Crosse, WI",
-                info: "Founder. Learnt a ton from other developers and geeks in the area",
+                info: "Founder. Learnt a ton from other developers and geeks in the area.",
                 meta: "Sept 2017 - Nov 2018"
             },
             {
                 title: "Linux Users Group",
                 location: "La Crosse, WI",
-                info: "Talking Linux with other enthusiasts",
+                info: "Talking Linux with other enthusiasts.",
                 meta: "Sept 2017 - Nov 2018"
             },
         ]
