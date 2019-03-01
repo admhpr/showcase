1f1ddd4c6f8e84e6f0ddd64fee260e866bb5a68f
Adam Harpur <adam@harpur.io>
2019-03-01 08:35:14 -0600

    feat: created readme.vue

---
+++ pages/readme.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <pre>     
+      ## Some context
+
+      This is a way for me to give a recruiter, manager, team mate (present or future) some context of who I am and what I'm about.
+      
+      ## About Me
+
+      This is not a resume, just a high level over view.
+
+      ### Personal
+
+      - I have travelled extensively and lived in a number of countries and states.
+
+      - I love to snowboard, surf and wakeboard and don't do it nearly as much as I'd like to.
+
+      - I value family, friends, creativity, thought, care, exercise, disciplined practice, meditation.
+
+      - I want to have a meaningful live and postively impact others around me.
+
+      ### Professional
+
+      - I am full stack developer, but often find myself tinkering with the frontend.
+
+      - My primary language is JavaScript. I can also write PHP, Python and I am happy to learn any other language or framework aslong as I am given the time and resources to do so.
+
+      - I have used strongly typed languages and enjoy using them
+
+      - I like to work in teams that communicate well and open the doors to collabaration.
+
+      - A big motivator for me is being given a small section of the work week to improve my skillset relevant to the job at hand.
+
+      - I really do love writing code and always strive to make it readable and maintainable. 
+      
+      ## My Current Toolbelt:
+
+      - Node on the server side. I am open to new approaches and languages.
+      
+      - Databases - whatever SQL or NoSQL is just fine.
+      
+      - I quite like Vue on the front end but have used React alongside TypeScript.
+
+      - I generally deploy to Digital Ocean, but would like to learn AWS.
+
+      - For managing projects, I just fit-in with whatever the team is using.
+
+      - I use Linux for an OS, I can use other systems but I would rather not.
+    
+
+      That's about it, thank you!
+
+      </pre>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
