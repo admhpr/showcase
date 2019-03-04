8d38454eef0f5345be0857f3c8d4e0611faea315
Adam Harpur <adam@harpur.io>
2019-03-04 06:03:49 -0600

    chore: adjustments to portfolio data

--- data/portfolio.js
+++ data/portfolio.js
@@ -3,19 +3,16 @@ import {
 } from "./common"
 export default [{
         title: "Experimental",
-        info: `${imgBase}experimental.png`,
         location: "https://codinglab.online/",
         meta: "View"
     },
     {
         title: "Writing",
-        info: `${imgBase}experimental.png`,
         location: "https://blog.harpur.io",
         meta: "View",
     },
     {
         title: "Open Source",
-        info: `${imgBase}experimental.png`,
         location: "https://github.com/harps116",
         meta: "View"
     },
