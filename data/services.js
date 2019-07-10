59e6ae7a9bb98ad3ddd975328dfcfb5d8bfe2427
Adam Harpur <adam@harpur.io>
2019-07-10 09:50:53 -0500

    chore: fix typo

--- data/services.js
+++ data/services.js
@@ -1,17 +1,19 @@
-export default [{
-        title: "Web Development",
-        info: "Build the site of you dreams using the latest and greatest tech."
-    },
-    {
-        title: "Frontend Contracting",
-        info: "Let me help your company take things to the next level."
-    },
-    {
-        title: "Scripts and tooling",
-        info: "Need to grab some hard to reach data, watch a website for keywords or another bespoke problem that needs solved."
-    },
-    {
-        title: "Lessons",
-        info: "Learn some web development skills or some general programming."
-    },
-]
+export default [
+  {
+    title: "Web Development",
+    info: "Build the site of your dreams using the latest and greatest tech."
+  },
+  {
+    title: "Frontend Contracting",
+    info: "Let me help your company take things to the next level."
+  },
+  {
+    title: "Scripts and tooling",
+    info:
+      "Need to grab some hard to reach data, watch a website for keywords or another bespoke problem that needs solved."
+  },
+  {
+    title: "Lessons",
+    info: "Learn some web development skills or some general programming."
+  }
+];
