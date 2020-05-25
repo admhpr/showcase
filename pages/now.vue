2bcabb3b462edacc41900d24e5f26ef3780eddd9
adam <adam@harpur.io>
2020-05-25 08:33:16 +0100

    chore: updates to now page

--- pages/now.vue
+++ pages/now.vue
@@ -4,21 +4,18 @@
     <section-container title="Now">
       <template v-slot:content>
         <div class="row">
-          <div class="col-md-10">
+          <div class="col-md-10 now">
             <p>
               This is a
               <a href="https://nownownow.com/about">now page</a>..
             </p>
             <p>
-              Right now, I'm making a git commit everyday, I've been doing it for a while and it's a good way keep me
-              accountable.
-              Trying to find my groove and what I really want to do in regards to programming and my career. Having a
-              blast doing soon.
+              Right now (25/05/20), I'm focusing on becoming a more complete developer, that means learning devops, automation and architecture.
             </p>
             <p>
-              Looking at React, TypeScript, more Vue and Node. Moving more into the JavaScript world seems
+              Looking at React, TypeScript, more Vue and Node (and now Deno). Moving more into the JavaScript world seems
               inevitable, but I want to keep my eye on other languages and keep broadening my understanding of programming as a
-              complete discipline.
+              complete discipline. Lower level languages that I have no experience with seem like a good opinion for learning, such as <a href="https://www.rust-lang.org/">Rust</a>.
             </p>
             <br>
             <p>
@@ -42,5 +39,8 @@ export default {
 };
 </script>
 
-
-
+<style scoped>
+.now {
+  padding: 1em 10em;
+}
+</style>
